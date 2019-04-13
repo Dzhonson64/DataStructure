00ebe3e539a3a5a4f6e10152ca21f446e15fc6e4
Dzhonson64 <31271622+Dzhonson64@users.noreply.github.com>
2019-04-13 14:10:49 +0400

    deploy

---
+++ DynamicArray/DynamicArray/DynamicArray.cpp
@@ -0,0 +1,68 @@
+﻿// DynamicArray.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "pch.h"
+#include <iostream>
+using namespace std;
+struct DArray {
+	int index = 0;
+	int capacity = 2;
+	int * dynamicArray = new int[capacity];
+}dArray;
+
+
+void push(int item);
+int getElemet(int id);
+int size();
+int pop();
+void getIndex(int item);
+int main()
+{
+	struct DArray * dArr = &dArray;
+	for (int i = 0; i < 5; i++)
+	{
+		push(10);
+	}
+	push(25);
+	//std::cout << pop();
+	for (int i = 0; i < 6; i++) {
+		cout << getElemet(i) << endl;
+	}
+}
+void push(int item){
+	struct DArray * dArr = &dArray;
+
+	if (dArr->index == dArr->capacity) {
+		int * arrayBuffer = new int[dArr->capacity * 2];
+		for (int i = 0; i < dArr->capacity; i++)
+		{
+			arrayBuffer[i] = dArr->dynamicArray[i];
+		}
+		delete [] dArr->dynamicArray;
+		
+		dArr->dynamicArray = arrayBuffer;
+
+		
+	}
+	cout << "dArr->capacity - " << dArr->capacity << endl;
+	dArr->dynamicArray[dArr->index] = item;
+	dArr->index++;
+}
+int getElemet(int id) {
+	struct DArray * dArr = &dArray;
+	return dArr->dynamicArray[id];
+}
+int size() {
+	struct DArray * dArr = &dArray;
+	return dArr->index;
+}
+int pop() {
+	struct DArray * dArr = &dArray;
+	int lastElem = dArr->dynamicArray[dArr->index - 1];
+	dArr->index -= 1;
+	return lastElem;
+}
+void getIndex(int item) {
+	struct DArray * dArr = &dArray;
+	
+}
